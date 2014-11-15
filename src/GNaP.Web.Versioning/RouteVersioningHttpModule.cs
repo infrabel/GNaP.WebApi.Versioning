314bd261176484fde642b7c9b207d3f6bcf3bc22
David Cumps <david@cumps.be>
2014-11-15 23:57:45 +0100

    Added Versioning implementation

---
+++ src/GNaP.Web.Versioning/RouteVersioningHttpModule.cs
@@ -0,0 +1,37 @@
+﻿namespace GNaP.Web.Versioning
+{
+    using System.Web;
+    using System.Web.Http;
+
+    internal class RouteVersioningHttpModule : IHttpModule
+    {
+        private static readonly object ApplicationStartLock = new object();
+
+        private static volatile bool _applicationStarted;
+
+        public void Init(HttpApplication context)
+        {
+            if (!_applicationStarted)
+            {
+                lock (ApplicationStartLock)
+                {
+                    if (!_applicationStarted)
+                    {
+                        SetupAttributeBasedRouting();
+                        _applicationStarted = true;
+                    }
+                }
+            }
+        }
+
+        private static void SetupAttributeBasedRouting()
+        {
+            if (GlobalConfiguration.Configuration.Routes.Count == 0)
+                GlobalConfiguration.Configure(config => config.MapHttpAttributeRoutes());
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
