572e987c73dc631ccfb80012f4bc5d9d37a21e0d
David Cumps <david@cumps.be>
2014-12-05 18:17:11 +0100

    No more trying to autofix MapHttpAttributeRoutes()

--- nupack.cmd
+++ nupack.cmd
@@ -1,7 +1,4 @@
 @ECHO OFF
 
 ECHO Packing GNaP.WebApi.Versioning
-nuget pack src\GNaP.WebApi.Versioning\GNaP.WebApi.Versioning.csproj -Prop Configuration=Release
-
-ECHO Packing GNaP.WebApi.Versioning.SystemWeb
-nuget pack src\GNaP.WebApi.Versioning.SystemWeb\GNaP.WebApi.Versioning.SystemWeb.csproj -Prop Configuration=Release
+nuget pack src\GNaP.WebApi.Versioning\GNaP.WebApi.Versioning.csproj -Build -Prop Configuration=Release -Exclude gnap.ico
