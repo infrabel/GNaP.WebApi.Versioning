165dec83d2f2b59fa3b72375d86f085b125deeaf
Infrabel <dotnet@infrabel.be>
2015-01-07 12:15:43 +0100

    Bumped to 1.0.6

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -4,9 +4,9 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: AssemblyProduct("GNaP.WebApi.Versioning")]
 [assembly: AssemblyDescription("Simplify versioning on your Web API routes by automatically supporting various versioning schemes.")]
-[assembly: AssemblyCopyright("Copyright (c) 2014 Infrabel")]
+[assembly: AssemblyCopyright("Copyright (c) 2014-2015 Infrabel")]
 [assembly: AssemblyCompany("Infrabel")]
 [assembly: AssemblyConfiguration("Release")]
 
-[assembly: AssemblyVersion("1.0.5.0")]
-[assembly: AssemblyFileVersion("1.0.5.0")]
+[assembly: AssemblyVersion("1.0.6.0")]
+[assembly: AssemblyFileVersion("1.0.6.0")]
